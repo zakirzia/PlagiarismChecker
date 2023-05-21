ea8b1b0d7a010cd15bb27268493e239f393e20f0
peacemaker <r.zakirzianova@lancaster.ac.uk>
2023-05-21 16:00:49 +0100

    Define Main class. HTML for Word Frequencies and Phrase Matching, gets threshold from the user input.

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
         return final_res;
     }
 
-    public static void HTMLPageForWordFrequency(LinkedHashMap<String, Integer> sortedWordFrequency1, LinkedHashMap<String, Integer> sortedWordFrequency2, String file1, String file2) {
+    public static void HTMLForWordFrequency(LinkedHashMap<String, Integer> sortedWordFrequency1, LinkedHashMap<String, Integer> sortedWordFrequency2, String file1, String file2, LinkedHashMap<String, Integer> phraseMatchRes) {
         try {
             String filename1 = new File(file1).getName();
             String filename2 = new File(file2).getName();
@@ -68,6 +68,18 @@ public class Main {
             }
 
             writer.println("</table>");
+
+            writer.println("<h1>Phrase Matching: " + filename1 + " and " + filename2 + "</h1>");
+            writer.println("<table border='1'>");
+            writer.println("<tr><th>Pair</th><th>Phrase Match (%)</th></tr>");
+
+            String matchPercentage = Integer.toString(phraseMatchRes.getOrDefault(file1 + " vs " + file2, 0));
+            writer.println("<tr>");
+            writer.println("<td>" + filename1 + " - " + filename2 + "</td>");
+            writer.println("<td>" + matchPercentage + "</td>");
+            writer.println("</tr>");
+
+            writer.println("</table>");
             writer.println("</body></html>");
 
             writer.close();
@@ -76,9 +88,77 @@ public class Main {
         }
     }
 
+    public static void HTMLForPhraseMatching(ArrayList<String> files, Checker checker) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileOutputStream("phraseMatching.html"));
+
+            writer.println("<html>");
+            writer.println("<head><title>Phrase Matching</title></head>");
+            writer.println("<body>");
+
+            for (String file : files) {
+                String fileName = new File(file).getName();
+                writer.println("<h1>Phrase Matching for file: " + fileName + "</h1>");
+                LinkedHashMap<String, Integer> res = sortMap(phraseMatchResultPerFile(checker, file, files));
+
+                writer.println("<table border='1'>");
+                writer.println("<tr><th>Pair</th><th>Phrase Match (%)</th></tr>");
+
+                for (Map.Entry<String, Integer> entry : res.entrySet()) {
+                    String otherFileName = new File(entry.getKey().split(" vs ")[1]).getName();
+                    String href = fileName + "_" + otherFileName + "_wordFrequency.html";
+                    writer.println("<tr>");
+                    writer.println("<td><a href='" + href + "'>" + fileName + " - " + otherFileName + "</a></td>");
+                    writer.println("<td>" + entry.getValue() + "</td>");
+                    writer.println("</tr>");
+                }
+                writer.println("</table><br>");
+            }
+
+            writer.println("<h1>Overall Phrase Matches:</h1>");
+
+            writer.println("<table border='1'>");
+            writer.println("<tr><th>Pair</th><th>Phrase Match (%)</th></tr>");
+
+            HashMap<String, Integer> overallRes = new HashMap<>();
+            for (String file1 : files) {
+                String fileName1 = new File(file1).getName();
+                for (String file2 : files) {
+                    String fileName2 = new File(file2).getName();
+                    if (!file1.equals(file2)) {
+                        LinkedHashMap<String, Integer> res = phraseMatchResultPerFile(checker, file1, files);
+                        overallRes.put(fileName1 + " - " + fileName2, res.get(file1 + " vs " + file2));
+                    }
+                }
+            }
+
+            LinkedHashMap<String, Integer> sortedOverallRes = sortMap(overallRes);
+            for (Map.Entry<String, Integer> entry : sortedOverallRes.entrySet()) {
+                String[] fileNames = entry.getKey().split(" - ");
+                String href = fileNames[0] + "_" + fileNames[1] + "_wordFrequency.html";
+                writer.println("<tr>");
+                writer.println("<td><a href='" + href + "'>" + entry.getKey() + "</a></td>");
+                writer.println("<td>" + entry.getValue() + "</td>");
+                writer.println("</tr>");
+            }
+
+            writer.println("</table>");
+
+            writer.println("</body></html>");
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
+        // Get the threshold from the user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the threshold value:");
+        int threshold = scanner.nextInt();
+
         ArrayList<String> files = new ArrayList<>(Arrays.asList("/Users/peacemaker/IdeaProjects/PlagiarismChecker/src/test1.txt", "/Users/peacemaker/IdeaProjects/PlagiarismChecker/src/test2.txt", "/Users/peacemaker/IdeaProjects/PlagiarismChecker/src/test3.txt"));
-        Checker checker = new Checker(files, 3);
+        Checker checker = new Checker(files, threshold);
 
         for (String file : files) {
             System.out.println("For file " + file);
@@ -88,13 +168,19 @@ public class Main {
         }
 
         for (int i = 0; i < files.size(); i++) {
-            for (int j = i + 1; j < files.size(); j++) {
-                ArrayList<HashMap<String, Integer>> frequencies = checker.compareWordFrequency(files.get(i), files.get(j));
-                LinkedHashMap<String, Integer> sortedFreq1 = sortMap(frequencies.get(0));
-                LinkedHashMap<String, Integer> sortedFreq2 = sortMap(frequencies.get(1));
+            for (int j = 0; j < files.size(); j++) {
+                if (i != j) {
+                    ArrayList<HashMap<String, Integer>> frequencies = checker.compareWordFrequency(files.get(i), files.get(j));
+                    LinkedHashMap<String, Integer> sortedFreq1 = sortMap(frequencies.get(0));
+                    LinkedHashMap<String, Integer> sortedFreq2 = sortMap(frequencies.get(1));
 
-                HTMLPageForWordFrequency(sortedFreq1, sortedFreq2, files.get(i), files.get(j));
+                    LinkedHashMap<String, Integer> phraseMatchRes = phraseMatchResultPerFile(checker, files.get(i), files);
+
+                    HTMLForWordFrequency(sortedFreq1, sortedFreq2, files.get(i), files.get(j), phraseMatchRes);
+                }
             }
         }
+
+        HTMLForPhraseMatching(files, checker);
     }
 }
